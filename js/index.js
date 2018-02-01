0cf48f30bd738216f5ea4d448f4e4b83a7ac7ec4
Emanuel Cerqueira <emanuelcerqueira07@gmail.com>
2018-02-01 16:21:32 -0300

    Refatorando e deixando de usar id

--- js/index.js
+++ js/index.js
@@ -1,111 +1,101 @@
 const numeroExtenso = {
-    unidades: [
-        'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove'
-    ],
-    dezenas: [
-        'dez', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta', 'noventa'
-    ],
-    dezenasDez: [
-        'onze', 'doze', 'treze', 'quartoze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove'
-    ],
-    centenas: [
-        'cento', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos', 'setecentos', 'oitocentos', 'novecentos'
-    ]
+  unidades: [
+    'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove'
+  ],
+  dezenas: [
+    'dez', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta', 'noventa'
+  ],
+  dezenasDez: [
+    'onze', 'doze', 'treze', 'quartoze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove'
+  ],
+  centenas: [
+    'cento', 'duzentos', 'trezentos', 'quatrocentos', 'quinhentos', 'seiscentos', 'setecentos', 'oitocentos', 'novecentos'
+  ]
 };
 
 const escalasPlural = ['mil', 'milhões', 'bilhões', 'trilhões', 'quatrilhões', 'quintilhões', 'sextilhões', 'septilhões', 'octilhões'];
 
-function quantidadeCentenasDeTres(numero) {
-    const qtdCentenas = Math.trunc(parseInt(numero.toString().length) / 3);
-    return qtdCentenas;
-}
-
-function isNaoSomenteCentenasDeTres(numero) {
-    const qtdNumerosRestantes = parseInt(numero.toString().length) - quantidadeCentenasDeTres(numero) * 3;
-    return qtdNumerosRestantes === 0 ? false : qtdNumerosRestantes;
-}
-
 function isDezenaDez(numero) {
-    return numero > 10 && numero < 20;
+  return numero > 10 && numero < 20;
 }
 
 function revertStr(str) {
-    return str.split('').reverse().join('');
+  return str.split('').reverse().join('');
 }
 
 function ordernarEmCentenas(numero) {
-    const strNumeroReverted = revertStr(numero.toString());
-    const regexCentenas = /\d\d?\d?/g;
+  const strNumeroReverted = revertStr(numero.toString());
+  const regexCentenas = /\d\d?\d?/g;
+  return strNumeroReverted.match(regexCentenas).reverse().map(item =>
+    parseInt(revertStr(item))
+  );
+}
 
-    return strNumeroReverted.match(regexCentenas).reverse().map(item =>
-        parseInt(revertStr(item))
-    );
+function centenaPorExtenso(centenaStr) {
+  let centenaExtenso = '';
+  centenaExtenso += numeroExtenso.centenas[centenaStr[0] - 1];
+  centenaExtenso += dezenaPorExtenso(centenaStr, 1);
+  return centenaExtenso;
+}
+
+function dezenaPorExtenso(centenaStr, startIndex) {
+  let dezenaExtenso = parseInt(centenaStr.slice(startIndex)[0]) == 0 || centenaStr.length === 2 ? '' : ' e ';
+
+  if (isDezenaDez(centenaStr.slice(startIndex))) {
+    dezenaExtenso += numeroExtenso.dezenasDez[centenaStr.slice(startIndex) - 11];
+  }
+  else {
+    dezenaExtenso += centenaStr[0 + startIndex] - 1 !== -1 ? numeroExtenso.dezenas[centenaStr[0 + startIndex] - 1] : '';
+    dezenaExtenso += centenaStr[1 + startIndex] - 1 !== -1 ? ' e ' +unidadePorExtenso(centenaStr, 1 + startIndex) : '';
+  }
+  return dezenaExtenso;
+}
+
+function unidadePorExtenso(centenaStr, index) {
+  return numeroExtenso.unidades[centenaStr[index] - 1];
 }
 
 function centenasExtenso(centenas) {
-    const centenasPorExtenso = centenas.map(element => {
-        let el = element.toString();
-        let porExtenso = '';
-        let dezena;
+ return centenas.map(centena => {
+    let centenaStr = centena.toString();
 
-        if (el.length === 3) {
-            dezena = el.slice(1);
-            porExtenso = numeroExtenso.centenas[el[0] - 1];
-    
-            if (isDezenaDez(dezena)) {
-                porExtenso += ' e '+numeroExtenso.dezenasDez[dezena - 11];
-            }
-            else {
-                porExtenso += el[1] - 1 !== -1 ? ' e '+numeroExtenso.dezenas[el[1] - 1] : '';
-                porExtenso += el[2] - 1 !== -1 ? ' e '+numeroExtenso.unidades[el[2] - 1]: '';
-            }
-        }
-        else if (el.length === 2) {
-            dezena = element;
-        
-            if (isDezenaDez(dezena)) {
-                porExtenso = numeroExtenso.dezenasDez[dezena - 11];
-            }
-            else {
-                porExtenso += el[0] - 1 !== -1 ? numeroExtenso.dezenas[el[0] - 1] : '';
-                porExtenso += el[1] - 1 !== -1 ? ' e '+numeroExtenso.unidades[el[1] - 1]: '';
-            }
-        }
-        else if (el.length === 1) {
-            porExtenso = numeroExtenso.unidades[element - 1];
-        }
-
-        return porExtenso; 
-    });
-
-    return centenasPorExtenso;
+    if (centenaStr.length === 3) {
+      return centenaPorExtenso(centenaStr);
+    }
+    else if (centenaStr.length === 2) {
+      return dezenaPorExtenso(centenaStr, 0);
+    }
+    else if (centenaStr.length === 1) {
+      return unidadePorExtenso(centenaStr, 0);
+    }
+  });
 }
 
 function numeroPorExtenso(centenas) {
-    const numExtenso = centenas.map((item, index) => {
-        if (item) {
-            let num = centenas.length - index - 1 !== 0 ? 
-            item+' '+escalasPlural[centenas.length - index - 2] : item;
-        
-            return num;
-        }     
-    }).filter(item => item !== undefined).map((item, index, arr) => {
-        if (index < arr.length - 2) {
-            item += ', ';
-        }
-        else if (index === arr.length - 2){
-            item += ' e ';
-        }
-        else {
-            item += ' ';
-        }
-        
-        return item;
-    });
+  const numExtenso = centenas.map((item, index) => {
+    if (item) {
+      let num = centenas.length - index - 1 !== 0 ?
+        item + ' ' + escalasPlural[centenas.length - index - 2] : item;
 
-    return numExtenso.join('');
+      return num;
+    }
+  }).filter(item => item !== undefined).map((item, index, arr) => {
+    if (index < arr.length - 2) {
+      item += ', ';
+    }
+    else if (index === arr.length - 2) {
+      item += ' e ';
+    }
+    else {
+      item += ' ';
+    }
+
+    return item;
+  });
+
+  return numExtenso.join('');
 }
 
 function converterNumeroParaExtenso(numero) {
-    return numeroPorExtenso(centenasExtenso(ordernarEmCentenas(numero)));
+  return numeroPorExtenso(centenasExtenso(ordernarEmCentenas(numero)));
 }
